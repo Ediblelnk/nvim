4ad8ece79f63b6c9c41f6031bdb06fb4dc20ebdb
Ediblelnk <peters1731@gmail.com>
2025-02-04 15:36:45 -0500

    color scheme

--- init.lua
+++ init.lua
@@ -16,7 +16,9 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {}
+local plugins = {
+    {"catppuccin/nvim", name = "catppuccin", priority = 1000}
+}
 local opts = {}
-
+ 
 require("lazy").setup(plugins, opts)
