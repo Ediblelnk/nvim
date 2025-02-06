8991e3a6c5d6c73a9ae9e0bccd08c44077870454
Ediblelnk <peters1731@gmail.com>
2025-02-06 16:30:45 -0500

    modularization + plugins

--- init.lua
+++ init.lua
@@ -2,32 +2,17 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
+vim.g.mapleader = " "
 
+-- use lazy, install if not installed already
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", --latest stable release
-        lazypath,
-    })
+    vim.fn.system(
+        {"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
+         lazypath})
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-    {"catppuccin/nvim", name = "catppuccin", priority = 1000},
-    {
-        'nvim-telescope/telescope.nvim', tag = '0.1.5',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    }
-}
 local opts = {}
- 
-require("lazy").setup(plugins, opts)
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
+require("lazy").setup("plugins")
